920a1548698dbaf20d9ec4d3151095a3775fba82
Fúlvio <fulviocanducci@hotmail.com>
2018-08-03 23:37:44 -0300

    5 next

---
+++ src/app/photos/photo-list/photos/photos.component.ts
@@ -0,0 +1,37 @@
+import { Photo } from './../../photo/photo';
+import { Component, OnInit, Input, OnChanges, SimpleChanges } from '@angular/core';
+
+@Component({
+  selector: 'app-photos',
+  templateUrl: './photos.component.html',
+  styleUrls: ['./photos.component.css']
+})
+export class PhotosComponent implements OnInit, OnChanges {
+
+
+  @Input() photos: Photo[] = [];
+  rows: any[] = [];
+  constructor() { }
+
+  ngOnInit() {
+
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes.photos){
+      this.rows = this.groupColumns(this.photos);
+    }
+  }
+
+  groupColumns(photos: Photo[]): any[] {
+      const n: any[] = [];
+      if (photos) {
+        for (let i = 0; i < photos.length; i = i + 3) {
+          n.push(photos.slice(i, i + 3));
+        }
+      }
+      return n;
+  }
+
+
+}
